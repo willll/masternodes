67a83a3a47375d4b1a570d83272e4818d487ff8c
willll <wilfried.fauvel@gmail.com>
2019-01-23 22:19:42 -0500

    More merges !!!

--- rest.py
+++ rest.py
@@ -1,5 +1,9 @@
+import json
 from fabric import Connection
 from klein import Klein
+from twisted.web.static import File
+from invoke.exceptions import UnexpectedExit
+from twisted.internet.defer import succeed
 import jinja2
 from coin import Coin,Polis
 from vps import VPS
@@ -7,13 +11,51 @@ from config import config,logging
 
 app = Klein()
 
+'''
+asynchronous do cli action
+'''
+def async_dacli(masternode, action, coin = "Polis"):
+    # noinspection PyBroadException
+    try:
 
-'''
-'''
-@app.route('/')
-def hello_world(request):
-    return 'Hello, World! <a href="/mns/list">Masternodes</a>' \
-           '<a href="/daemon/startpolis">Start Polis</a> <a href=''>Masternodes</a> '
+        kwargs = {}
+        if "connection_certificate" in masternode :
+            kwargs['key_filename'] = masternode["connection_certificate"]
+        else:
+            # Must be mutually excluded
+            if "password" in masternode:
+                kwargs['password'] = masternode["password"]
+
+        connection = Connection(masternode["connection_string"],  connect_timeout=31, connect_kwargs=kwargs)
+        logging.info('>>> Got connection to {} using {} '.format(masternode["connection_string"], kwargs))
+
+        if "destination_folder" in masternode:
+            conx_str = '{}/{}'.format( masternode["destination_folder"], config[coin]["cli"])
+        elif "default_dir" in config[coin]:
+            conx_str = '{}/{}'.format(config[coin]["default_dir"], config[coin]["cli"])
+        else:
+            conx_str = config[coin]["cli"]
+
+        if "wallet_directory" in masternode :
+            wallet_dir = masternode["wallet_directory"]
+            conx_str += " --datadir=" + wallet_dir
+        elif config[coin]["default_wallet_dir"]:
+            conx_str += " --datadir=" + config[coin]["default_wallet_dir"]
+
+        conx_str += " "+action
+        result = connection.run(conx_str, hide=False)
+        logging.info("Executed {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}".format(result))
+
+        connection.close()
+        logging.info('Connection closed'.format(masternode["connection_string"]))
+        return result.stdout
+    except UnexpectedExit as e:
+        #possibly try to start  the daemon again
+        logging.warning('{} exited unexpectedly'.format(config[coin]["cli"]), exc_info=e)
+        return '{"status":"restart"}'
+    except Exception as e:
+        logging.error('Could not do_action {} : {}'.format(masternode["connection_string"], e), exc_info=e)
+        return 'exception'
 
 '''
 Temporary fix for templating
@@ -32,6 +74,7 @@ def render_without_request(template_name, **template_vars):
     )
     template = env.get_template(template_name)
     return template.render(**template_vars)
+
 '''
 '''
 def shell_actions(action, masternode):
@@ -162,7 +205,7 @@ REST endpoint to clean up wallet dir (rm blockchain files) start daemon with -re
 TODO:
 '''
 
-'''
+''''
 Subroute of script installs (to crontab mostly)
 '''
 with app.subroute("/scripts") as scripts:
@@ -171,9 +214,36 @@ with app.subroute("/scripts") as scripts:
     '''
     @scripts.route('/watcher', methods=['GET'])
     def watcher_install(request):
-        mnidx = int(request.args.get(b'mnidx',[0])[0])
-        masternode = config["masternodes"][mnidx]
-        return VPS(masternode).install_watcher(Polis(config["Polis"]))
+        try:
+            mnidx = int(request.args.get(b'mnidx',[0])[0])
+            masternode = config["masternodes"][mnidx]
+            coin_name = "polis"
+            kwargs = {}
+            if "connection_certificate" in masternode:
+                kwargs['key_filename'] = masternode["connection_certificate"]
+            else:
+                # Must be mutually excluded
+                if "password" in masternode:
+                    kwargs['password'] = masternode['password']
+
+            connection = Connection(masternode["connection_string"],  connect_timeout=31, connect_kwargs=kwargs)
+            logging.info("Installing watcher on  {}".format(masternode["connection_string"]) )
+            polis = config["Polis"]
+
+            connection.put(polis["scripts"]["local_path"]+polis["scripts"]["watcher_cron"])
+            result = connection.run("/bin/bash {} {} {} {}".format(
+                polis["scripts"]["watcher_cron"], coin_name, polis["default_dir"], polis["daemon"],
+                polis["default_wallet_dir"]), hide=False)
+
+            connection.close()
+
+            if result.stdout == '' and result.stderr == '':
+                return "Watcher installed succesfully"
+
+            return "Failed stdout: {}\nstderr: {} ".format(result.stdout, result.stderr)
+        except Exception as e:
+            logging.error("Failed to install watcher: {} ".format(masternode["connection_string"]), exc_info = e)
+            return "Exception"
 
 '''
 Manage the config file from web
@@ -207,9 +277,12 @@ with app.subroute("/config") as conf:
     def config_view(request):
         return render_without_request()
 
-
-
-
+'''
+'''
+@app.route('/')
+def hello_world(request):
+    template = "home.html"
+    return render_without_request(template)
 
 '''
 Create a new MN:
@@ -230,36 +303,82 @@ def create(request):
         logging.info('ip = {}, password = {}, port = {}, name = {}'.format(request.args.get('ip'),
             password,request.args.get('port'),request.args.get('name')))
 
-        vps = VPS({
+        masternode = {
             "connection_sting": "{}@{}:{}".format(request.args.get('user'), request.args.get('host'),
                 request.args.get('port)')),
             "password":password,
-            "name":request.args.get("name") })
+            "name":request.args.get("name") }
+        kwargs['password'] = password
 
-        coin = Polis(config['Polis'])
+        try:
+        #upload scripts to host
+            coin_name = "Polis"
 
-        '''
-        does all the apt get
-        get polisd from another mn if not available locally (polis.tgz)
-        '''
-        result = vps.preconf(coin)
-        logging.info("Preconf done apt gets and made directorys, copied polis.tgz:\n{}".format(result))
+            polis = config[coin_name]
+            connection = Connection(masternode["connection_string"],  connect_timeout=31, connect_kwargs=kwargs)
+            # does all the apt get
+            result = connection.put(polis["preconf"])
+            #get polisd from another mn if not available locally (polis.zip)
+            """
+            polishash = hashlib.md5(open("polis.tgz","rb").read()).hexdigest()
+            if(polishash != config["polisHash"]):
+                #wrong version, need to get new one from top MN
+            """
 
-        result = vps.daemonconf(coin)
-        logging.info("Daemon configures\n{}".format(result))
+            connection.put(polis["preconf"]["version_to_upload"])
+            connection.run("mkdir {} && mkdir {} && tar zxvf {} {}".format( config["WalletsFolder"],
+                                                                           polis["default_dir"],
+                                                                           polis["version_to_upload"],
+                                                                           polis["default_dir"]), hide=False)
 
-        result = vps.install_watcher(Polis(config["Polis"]))
-        logging.info('Uploaded and ran watcher_cron.sh :\n {}'.format(result))
+            #second part configuration script, generates privkey and gets polisd running properly
+            # TODO: this can easily be all generated within the script and
+            # simply pasted into the remote .wallet file at location. Might
+            # require a polisd running locally though, to generate masternode
+            # privkey
+            result = connection.put(polis["confdaemon"] )
+            logging.info('Uploaded {}:\n {}'.format(polis["confdaemon"], result))
+            result = connection.run("/bin/bash {} {} {} {} {}".format(
+                polis["confdaemon"], coin_name, polis["addnode"],
+                polis["default_dir"], masternode["connection_string"].split("@")[1].split(":")[0] ), hide=False)
+            logging.info('Ran {}:\n {}'.format(polis["confdaemon"]), result)
 
-        result = vps.install_sentinel(coin)
-        logging.info('Uploaded and ran sentinel_setup.sh :\n {}'.format(result))
+            """
+            extract privkey from result here,
+            save it in msaternode["masternode_private_key"]
+            """
+            #Setup script which watches daemon and restarts it on crash
+            connection.put(polis["scripts"]["local_path"]+polis["watcher_cron"])
+            logging.info('Uploaded {}:\n {}'.format(polis["watcher_cron"], result))
+            result = connection.run("/bin/bash {} {} {} {}".format(
+                polis["scripts"]["watcher_cron"],coin_name, polis["default_dir"], polis["daemon"],
+                polis["default_wallet_dir"]), hide=False)
+            logging.info('Uploaded {}:\n {}'.format(polis["scripts"]["watcher_cron"], result))
 
-        config["masternodes"].append(masternode)
-        with open('config.json', 'w') as outfile:
-            json.dump(config, outfile)
 
-        #save masternode to config.json.
-        return result
+            #setup sentinel
+            connection.put(polis["sentinel_setup"])
+            logging.info('Uploaded {}:\n {}'.format(polis["sentinel_setup"], result))
+            result = connection.run("/bin/bash {} {} {} {}".format(polis["sentinel_setup"],
+                                               polis["sentnel_git"],
+                                               polis["default_dir"],
+                                               coin_name), hide=False)
+            logging.info('Uploaded {}:\n {}'.format(polis["sentinel_setup"], result))
+
+            config["masternodes"].append(masternode)
+            with open('config.json', 'w') as outfile:
+                json.dump(config, outfile)
+
+            connection.close()
+            #save masternode to config.json.
+            return result
+        except UnexpectedExit as e:
+            #possibly try to start polisd
+            logging.warning('{} exited unexpectedly'.format('polis-cli'), exc_info=e)
+            return "UnexpectedExit"
+        except Exception as e :
+            logging.error('Could not getinfo: {}'.format('polis-cli'), exc_info=e)
+            return "failed"
 
     else:
         template="new_mn.html"
